75aebf221df6fa6bba926716d6cca5670a75c5e6
Kenichi Kamiya <kachick1@gmail.com>
2019-06-14 15:05:29 +0900

    Finished ... :)

--- src/Main.elm
+++ src/Main.elm
@@ -37,4 +37,16 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    text "foo"
+    div []
+        [ Html.form [ onSubmit Submit ]
+            [ input [ value model.input, onInput Input ] []
+            , button
+                [ disabled (String.length model.input < 1) ]
+                [ text "Submit :)" ]
+            ]
+        , ul [] (List.map viewMemo model.memos)
+        ]
+
+viewMemo : String -> Html Msg
+viewMemo memo = 
+    li [] [ text memo ]
